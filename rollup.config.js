a91c8eaa2a0f282248f6294bd5e29fc93bf13061
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-26 15:46:29 +0900

    docs: rollup 설정 파일 추가

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/bundle.js",
+    format: "iife",
+  },
+  plugins: [resolve(), commonjs(), typescript(), terser()],
+};

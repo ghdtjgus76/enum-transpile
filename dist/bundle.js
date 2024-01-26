d474b341e88d1a0c763ee98c767ce87979acf14c
ghdtjgus76 <ghdtjgus76@naver.com>
2024-01-26 16:22:17 +0900

    feat: enum 트랜스파일

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+var E;
+!(function (E) {
+  (E.READY = "READY"),
+    (E.DELIVERING = "DELIVERING"),
+    (E.DELIVERED = "DELIVERED");
+})(E || (E = {})),
+  console.log(E.DELIVERED);
+//# sourceMappingURL=bundle.js.map
---
+++ src/enum.ts
@@ -0,0 +1,11 @@
+export enum DeliveryStatus1 {
+  READY = "READY",
+  DELIVERING = "DELIVERING",
+  DELIVERED = "DELIVERED",
+}
+
+export const enum DeliveryStatus2 {
+  READY = "READY",
+  DELIVERING = "DELIVERING",
+  DELIVERED = "DELIVERED",
+}
